a24c674755e1ce9b76921f42bfe723fcee3bd88c
JarkkoPar <64613837+JarkkoPar@users.noreply.github.com>
2024-02-15 20:33:40 +0100

    Added the GDScript code

---
+++ stack_machine.gd
@@ -0,0 +1,57 @@
+extends Node
+
+# This is the stack machine, which handles the overall game states. 
+# It creates the lowest-level states when needed. Each stack_state script
+# then can create what ever states they need to create.
+
+var state_stack:Array 		# The state stack
+var blackboard:Dictionary	# The blackboard to share data between states
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	initialize()
+
+
+func initialize() -> void:
+	blackboard  = {}
+	state_stack = []
+
+
+# Used to push a new state on to the stack.
+func push_state_to_stack( new_state:Node ) -> void:
+	new_state.stack_machine = self
+	new_state.blackboard = blackboard
+	
+	
+	state_stack.push_back(new_state)
+	add_child(new_state)
+	new_state.owner = self.owner
+	
+	new_state.on_state_pushed()
+
+
+# Used to pop the top-most state off the state stack.
+func pop_state_from_stack() -> void:
+	if len(state_stack) == 0:
+		return
+	var popped_state = state_stack.pop_back()
+	popped_state.on_state_popped()
+	remove_child(popped_state)
+	popped_state.queue_free()
+	if len(state_stack) > 0:
+		state_stack[-1].on_state_reactivated()
+
+
+# Called every physics frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if len(state_stack) == 0 :
+		return
+	
+	# Run the physics process for all the states in the stack.
+	for state in state_stack:
+		state.physics_process(delta)
+	
+	# We tick the top-most stack state.
+	state_stack[-1].tick_state(delta)
+
---
+++ stackable_state.gd
@@ -0,0 +1,50 @@
+extends Node 
+class_name stackable_state
+
+# This is the base state from which the other states are derived from. 
+# Because Godot uses duck-typing, this base state isn't strictly needed, 
+# but I like to use a base class to define a common interface when
+# creating class-like hierarchies.
+
+var stack_machine = null
+var blackboard:Dictionary 
+
+
+# This is called when the stack has been initialized and added
+# to the stack.
+func on_state_pushed() -> void:
+	pass
+
+
+# This is called when the stack has just been popped from the
+# stack, but before it is deleted with queue_free().
+func on_state_popped() -> void:
+	pass
+
+
+# This is called when the state on top of this state has just
+# been popped.
+func on_state_reactivated() -> void:
+	pass
+
+
+# This is called only for the top-most state in the stack.
+func tick_state(delta) -> void:
+	pass
+
+
+# This is ticked every physics frame, going from the state
+# at the bottom of the stack towards the one on top.
+func physics_process(delta) -> void:
+	pass
+
+# The following are convinience methods so that it is easier to 
+# push and pop the states within the stackable states.
+
+func push_state_to_stack( new_state:Node ) -> void:
+	stack_machine.push_state_to_stack(new_state)
+
+
+func pop_state_from_stack() -> void:
+	stack_machine.pop_state_from_stack()
+
